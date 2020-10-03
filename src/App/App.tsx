3a60444eb29ec233a1ad4ca5073a36e3abb5b294
Andreas Aaberge Eide <andreasaaeide@gmail.com>
2020-10-03 16:34:14 +0200

    Add StudioPicker

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -4,13 +4,18 @@ import Grid from '@material-ui/core/Grid';
 import { fetchBookingList, Booking, Studio } from '../api';
 
 import DatePicker from '../components/DatePicker';
+import StudioPicker from '../components/StudioPicker';
 import BookingPicker from '../components/BookingPicker';
 
 const App: React.FC = () => {
   const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+  const [selectedStudio, setSelectedStudio] = useState<Studio>(Studio.Studio1);
   const [currentBookingList, setCurrentBookingList] = useState<Booking[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
+  /**
+   * Fetch booking list whenever selectedDate changes.
+   */
   useEffect(() => {
     if (selectedDate !== null) {
       setIsLoading(true);
@@ -19,16 +24,20 @@ const App: React.FC = () => {
         selectedDate.getFullYear(),
         selectedDate.getMonth() + 1, // getMonth() returns 0-indexed month number
         selectedDate.getDate(),
-        Studio.Studio1,
+        selectedStudio,
       )
         .then((bookingList) => setCurrentBookingList(bookingList))
         .then(() => setIsLoading(false));
     }
-  }, [selectedDate]);
+  }, [selectedDate, selectedStudio]);
 
   return (
     <Grid container justify='space-around'>
       <DatePicker value={selectedDate} setValue={setSelectedDate} />
+      <StudioPicker
+        selectedStudio={selectedStudio}
+        selectStudio={setSelectedStudio}
+      />
       <BookingPicker
         bookingList={currentBookingList}
         disabled={isLoading || selectedDate === null}
---
+++ src/components/StudioPicker.tsx
@@ -0,0 +1,43 @@
+import React, { ChangeEvent } from 'react';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+
+import { Studio } from '../api';
+
+interface StudioPickerProps {
+  selectedStudio: Studio;
+  selectStudio: (s: Studio) => void;
+}
+
+const StudioPicker: React.FC<StudioPickerProps> = ({
+  selectedStudio,
+  selectStudio,
+}) => {
+  function handleChange(event: ChangeEvent<{ value: unknown }>): void {
+    selectStudio(event.target.value as Studio);
+  }
+
+  return (
+    <FormControl /* className={classes.formControl} */>
+      <Select
+        value={selectedStudio}
+        onChange={handleChange}
+        displayEmpty
+        /* className={classes.selectEmpty} */
+        inputProps={{ 'aria-label': 'Without label' }}
+      >
+        <MenuItem value='' disabled>
+          Velg studio
+        </MenuItem>
+        <MenuItem value={Studio.Studio1}>Studio 1</MenuItem>
+        <MenuItem value={Studio.Studio2}>Studio 2</MenuItem>
+        <MenuItem value={Studio.Autoavvikler}>Autoavvikler</MenuItem>
+      </Select>
+      <FormHelperText>Placeholder</FormHelperText>
+    </FormControl>
+  );
+};
+
+export default StudioPicker;
